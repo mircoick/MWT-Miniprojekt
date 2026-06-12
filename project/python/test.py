fe331e1e66a93a8c0172c48badab4ceda86ee005
Tassilo Hertling <tassilo.hertling@outlook.de>
2026-06-12 13:39:59 +0200

    tweak

---
+++ project/python/test.py
@@ -0,0 +1,13 @@
+phase_1= 77.35 - 70.19
+phase_2= 107.4 - 100.7
+
+phase_3= 80.47 - 72.63
+phase_4= 110.4 - 104.7
+
+print(phase_1)
+print(phase_2)
+print(abs(phase_1-phase_2))
+print("-----------------")
+print(phase_3)
+print(phase_4)
+print(abs(phase_3-phase_4))
